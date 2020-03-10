19d114634f2ee42445dfabe0665229fbd78482aa
Leo <xinlichao2016@gmail.com>
2020-03-10 09:38:46 +0800

    add utils

---
+++ utils/git-pull.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+##
+## git 批量更新当前目录下所有项目
+##
+## Started on 2020/03/10 Leo <xinlichao2016@gmail.com>
+## Last update 2020/03/10 Leo <xinlichao2016@gmail.com>
+##
+
+baseDir=$(pwd)
+#ls | while read file
+Dirs=$(ls -l | grep '^d' | awk '{print $9}')
+for file in $Dirs; do
+  echo $file
+  if [ -d $file ]; then
+    cd $file
+    git pull
+    cd $baseDir
+  fi
+done
